0e3f8b39d7cca1b2e2073e355d3b939d61df8e52
= <optikcrash@hotmail.com>
2022-05-14 14:37:25 -0700

    init

---
+++ app/src/main/java/com/digitalrelay/practice/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.digitalrelay.practice.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
